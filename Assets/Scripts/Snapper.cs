e64a59f0853d16124f2c39377e5b184b391b6c30
Anton <rozum.ant@gmail.com>
2022-06-13 19:11:27 +0300

    implemented simple snapping to whole coordinates

--- Assets/Scripts/Snapper.cs
+++ Assets/Scripts/Snapper.cs
@@ -1,9 +1,27 @@
 using UnityEditor;
+using UnityEngine;
 
 public static class Snapper
 {
+    private const string UNDO_SNAP = "snap objects";
+    
     [MenuItem("Edit/Snap selected objects")]
     public static void SnapTheThings()
     {
+        foreach (var go in Selection.gameObjects)
+        {
+            Undo.RecordObject(go.transform, UNDO_SNAP);
+            go.transform.position = go.transform.position.Round();
+        }
+    }
+
+    public static Vector3 Round(this Vector3 v)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            v[i] = Mathf.Round(v[i]);
+        }
+
+        return v;
     }
 }
