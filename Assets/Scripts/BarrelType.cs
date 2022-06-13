97a8aee49900f63440e6e4cde15a327d61bd6177
Anton <rozum.ant@gmail.com>
2022-06-13 18:13:55 +0300

    manual version

--- Assets/Scripts/BarrelType.cs
+++ Assets/Scripts/BarrelType.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class BarrelType : ScriptableObject
 {
-    [field: SerializeField, Range(1f, 8f)] public float Radius { get; private set; } = 1f;
-    [field: SerializeField] public float Damage { get; private set; } = 10f;
-    [field: SerializeField] public Color BarrelColor { get; private set; } = Color.red;
+    [field: SerializeField, Range(1f, 8f)] public float Radius { get; set; } = 1f;
+    [field: SerializeField] public float Damage { get; set; } = 10f;
+    [field: SerializeField] public Color BarrelColor { get; set; } = Color.red;
 }
--- Assets/Scripts/Editor/BarrelTypeEditor.cs
+++ Assets/Scripts/Editor/BarrelTypeEditor.cs
@@ -1,11 +1,21 @@
 using UnityEditor;
-using UnityEngine;
 
 [CustomEditor(typeof(BarrelType))]
 public class BarrelTypeEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        var barrel = target as BarrelType;
+
+        var newRadius = EditorGUILayout.FloatField("radius", barrel.Radius);
+
+        if (newRadius != barrel.Radius)
+        {
+            Undo.RecordObject(barrel, "change barrel radius");
+            barrel.Radius = newRadius;
+        }
         
+        barrel.Damage = EditorGUILayout.FloatField("damage", barrel.Damage);
+        barrel.BarrelColor = EditorGUILayout.ColorField("color", barrel.BarrelColor);
     }
 }
