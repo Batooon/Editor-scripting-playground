e5929086874526c051d4b9c012cf3d9e77f52100
Anton <rozum.ant@gmail.com>
2022-06-13 19:16:45 +0300

    validate function and hotkey implemented

--- Assets/Scripts/Snapper.cs
+++ Assets/Scripts/Snapper.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public static class Snapper
 {
     private const string UNDO_SNAP = "snap objects";
-    
-    [MenuItem("Edit/Snap selected objects")]
+
+    [MenuItem("Edit/Snap selected objects %&s", isValidateFunction: true)]
+    public static bool SnapTheThingsValidate()
+    {
+        return Selection.gameObjects.Length > 0;
+    }
+
+    [MenuItem("Edit/Snap selected objects %&s")]
     public static void SnapTheThings()
     {
         foreach (var go in Selection.gameObjects)
