be3d88ce10d265b98dcbba339ad776d95ae38e60
Anton <rozum.ant@gmail.com>
2022-06-14 01:31:57 +0300

    Grid snapping tool with visualization grid

--- Assets/Scripts/Editor/SnapperTool.cs
+++ Assets/Scripts/Editor/SnapperTool.cs
@@ -1,10 +1,18 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+
+//TODO: 2. малювати цю сітку з виставленим кроком(навколо вибраних об'єктів/показувати позицію переміщення після снаппінгу)
+//TODO: 3. додати підтримку для полярної системи координат
+//TODO: 4. зробити сітку зберігатися між сесіями Юніті(закрив і відкрив Юніті - сітка зберігла свої параметри)
 
 public class SnapperTool : EditorWindow
 {
     private const string UNDO_SNAP = "snap objects";
     
+    private float _gridStep = 1f;
+    private int _gridSize = 3;
+
     [MenuItem("Tools/Snapper")]
     private static void OpenSnapperTool() => GetWindow<SnapperTool>("Snapper");
 
@@ -22,11 +30,40 @@ public class SnapperTool : EditorWindow
 
     private void DuringSceneGUI(SceneView sceneView)
     {
-        Handles.DrawLine(Vector3.zero, Vector3.up);
+        if (Selection.gameObjects.Length == 0)
+            return;
+        Handles.zTest = CompareFunction.Less;
+        var offset = _gridStep * .5f;
+        Handles.color = Color.black;
+
+        foreach (var obj in Selection.gameObjects)
+        {
+            var pos = obj.transform.position;
+            var center = GetSnappedPosition(pos);
+
+            for (var i = 0; i < _gridSize * 2 + 1; i++)
+            {
+                // var y = (i - _gridSize) * _gridStep + center.y;
+                
+                var z = (i-_gridSize)*_gridStep + center.z;
+                var p1 = new Vector3(center.x - _gridSize * _gridStep - offset, center.y, z);
+                var p2 = new Vector3(center.x + _gridSize * _gridStep + offset, center.y, z);
+                Handles.DrawLine(p1, p2);
+
+                var x = (i-_gridSize) * _gridStep + center.x;
+                p1.Set(x, center.y, center.z + _gridSize * _gridStep + offset);
+                p2.Set(x, center.y, center.z - _gridSize * _gridStep - offset);
+                Handles.DrawLine(p1, p2);
+            }
+        }
+
+        Handles.color = Color.white;
     }
 
     private void OnGUI()
     {
+        _gridStep = EditorGUILayout.FloatField("Grid step", _gridStep);
+        _gridSize = EditorGUILayout.IntField("Grid size", _gridSize);
         using (new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0))
         {
             if (GUILayout.Button("SnapSelection"))
@@ -41,7 +78,31 @@ public class SnapperTool : EditorWindow
         foreach (var go in Selection.gameObjects)
         {
             Undo.RecordObject(go.transform, UNDO_SNAP);
-            go.transform.position = go.transform.position.Round();
+
+            var newPosition = go.transform.position;
+            go.transform.position = GetSnappedPosition(newPosition);
         }
+    }
+
+    private Vector3 GetSnappedPosition(Vector3 pos)
+    {
+        var newPosition = pos;
+        for (var i = 0; i < 3; i++)
+        {
+            var coordinate = newPosition[i];
+            var delta = coordinate % _gridStep;
+            if (delta > _gridStep * .5f)
+            {
+                coordinate += _gridStep - delta;
+            }
+            else
+            {
+                coordinate -= delta;
+            }
+
+            newPosition[i] = coordinate;
+        }
+
+        return newPosition;
     }
 }
