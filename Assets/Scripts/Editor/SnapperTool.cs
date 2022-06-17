a60a2d78e9b2312b603bc2f2b589d4c9f120c75a
Anton <rozum.ant@gmail.com>
2022-06-17 21:47:26 +0300

    some editor changes

--- Assets/Scripts/Editor/SnapperTool.cs
+++ Assets/Scripts/Editor/SnapperTool.cs
@@ -9,18 +9,28 @@ public class SnapperTool : EditorWindow
 {
     private const string UNDO_SNAP = "snap objects";
 
-    [SerializeField] private bool _polarGrid;
+    private enum GridType
+    {
+        Cartesian,
+        Polar
+    }
+    // [SerializeField] private bool _polarGrid;
+    [SerializeField] private GridType _gridType;
+    [SerializeField] private bool _useAngularDivision;
+    [SerializeField] private int _angularDivisions = 24;
     [SerializeField] private float _angleStep;
-    [SerializeField] private float _distanceStep;
+    // [SerializeField] private float _distanceStep;
     [SerializeField] private float _gridStep = 1f;
     [SerializeField] private float _cartesianExtent = 3f;
     [SerializeField] private float _polarExtent = 3f;
     private float _snapPositionIndicatorSize = .1f;
 
     private SerializedObject _so;
+    private SerializedProperty _propUseAngularDivision;
+    private SerializedProperty _propAngularDivisions;
     private SerializedProperty _propAngleStep;
-    private SerializedProperty _propDistanceStep;
-    private SerializedProperty _propPolarGrid;
+    // private SerializedProperty _propDistanceStep;
+    private SerializedProperty _propGridType;
     private SerializedProperty _propGridStep;
     private SerializedProperty _propCartesianExtent;
     private SerializedProperty _propPolarExtent;
@@ -31,9 +41,11 @@ public class SnapperTool : EditorWindow
     private void OnEnable()
     {
         _so = new SerializedObject(this);
+        _propUseAngularDivision = _so.FindProperty(nameof(_useAngularDivision));
+        _propAngularDivisions = _so.FindProperty(nameof(_angularDivisions));
         _propAngleStep = _so.FindProperty(nameof(_angleStep));
-        _propDistanceStep = _so.FindProperty(nameof(_distanceStep));
-        _propPolarGrid = _so.FindProperty(nameof(_polarGrid));
+        // _propDistanceStep = _so.FindProperty(nameof(_distanceStep));
+        _propGridType = _so.FindProperty(nameof(_gridType));
         _propGridStep = _so.FindProperty(nameof(_gridStep));
         _propCartesianExtent = _so.FindProperty(nameof(_cartesianExtent));
         _propPolarExtent = _so.FindProperty(nameof(_polarExtent));
@@ -54,97 +66,133 @@ public class SnapperTool : EditorWindow
             return;
         if (Event.current.type != EventType.Repaint)
             return;
-        
-        if (_propPolarGrid.boolValue)
+
+        switch (_gridType)
         {
-            var circlesAmount = Mathf.RoundToInt((_polarExtent * 2) / _distanceStep);
-            var linesCount = _angleStep > 0 ? Mathf.FloorToInt(360 / _angleStep) : 0;
-            var rotationAngle = new Vector3(0, _angleStep, 0);
-            var rotation = Matrix4x4.Rotate(Quaternion.Euler(rotationAngle));
-
-            Handles.color = Color.black;
-            Handles.zTest = CompareFunction.LessEqual;
-            for (var i = 1; i <= circlesAmount; i++)
+            case GridType.Polar:
             {
-                Handles.DrawWireDisc(Vector3.zero, Vector3.up, _distanceStep * i);
+                DrawPolarGrid();
+                break;
             }
-
-            var nextPoint = Vector3.right * (circlesAmount * _distanceStep);
-            for (var i = 0; i < linesCount; i++)
+            case GridType.Cartesian:
+            default:
             {
-                Handles.DrawAAPolyLine(Vector3.zero, nextPoint);
-                nextPoint = rotation * nextPoint;
-            }
-        }
-        else
-        {
-            var lineCount = Mathf.RoundToInt((_cartesianExtent * 2) / _gridStep);
-            if (lineCount % 2 == 0)
-                lineCount++;
-            var halfLineCount = lineCount / 2;
-            
-            foreach (var obj in Selection.gameObjects)
-            {
-                var pos = obj.transform.position;
-                var center = pos.Round(_gridStep);
-
-                Handles.color = Color.red;
-                Handles.zTest = CompareFunction.Always;
-                
-                Handles.SphereHandleCap(0,
-                    center,
-                    Quaternion.identity,
-                    _snapPositionIndicatorSize,
-                    EventType.Repaint);
-
-                Handles.color = Color.black;
-                Handles.zTest = CompareFunction.LessEqual;
-
-                for (var i = 0; i < lineCount; i++)
-                {
-                    var offset = i - halfLineCount;
-
-                    var x = offset * _gridStep + center.x;
-                    var z0 = center.z + halfLineCount * _gridStep;
-                    var z1 = center.z - halfLineCount * _gridStep;
-                    var p0 = new Vector3(x, center.y, z0);
-                    var p1 = new Vector3(x, center.y, z1);
-                    Handles.DrawAAPolyLine(p0, p1);
-
-                    x = offset * _gridStep + center.z;
-                    z0 = center.x + halfLineCount * _gridStep;
-                    z1 = center.x - halfLineCount * _gridStep;
-                    p0.Set(z0, center.y, x);
-                    p1.Set(z1, center.y, x);
-                    Handles.DrawAAPolyLine(p0, p1);
-                }
+                DrawCartesianGrid();
+                break;
             }
         }
 
         Handles.color = Color.white;
     }
 
+    private void DrawPolarGrid()
+    {
+        var circlesAmount = Mathf.RoundToInt(_polarExtent / _gridStep);
+        int linesCount;
+        Vector3 rotationAngle;
+        if (_useAngularDivision)
+        {
+            linesCount = _angularDivisions;
+            rotationAngle = Vector3.up * (360f / linesCount);
+        }
+        else
+        {
+            linesCount = _angleStep > 0 ? Mathf.FloorToInt(360 / _angleStep) : 0;
+            rotationAngle = Vector3.up * _angleStep;
+        }
+                
+        var rotation = Matrix4x4.Rotate(Quaternion.Euler(rotationAngle));
+
+        Handles.color = Color.black;
+        Handles.zTest = CompareFunction.LessEqual;
+        for (var i = 1; i <= circlesAmount; i++)
+        {
+            Handles.DrawWireDisc(Vector3.zero, Vector3.up, _gridStep * i);
+        }
+
+        var nextPoint = Vector3.right * (circlesAmount * _gridStep);
+        for (var i = 0; i < linesCount; i++)
+        {
+            Handles.DrawAAPolyLine(Vector3.zero, nextPoint);
+            nextPoint = rotation * nextPoint;
+        }
+    }
+
+    private void DrawCartesianGrid()
+    {
+        var lineCount = Mathf.RoundToInt((_cartesianExtent * 2) / _gridStep);
+        if (lineCount % 2 == 0)
+            lineCount++;
+        var halfLineCount = lineCount / 2;
+            
+        foreach (var obj in Selection.gameObjects)
+        {
+            var pos = obj.transform.position;
+            var center = pos.Round(_gridStep);
+
+            Handles.color = Color.red;
+            Handles.zTest = CompareFunction.Always;
+                
+            Handles.SphereHandleCap(0,
+                center,
+                Quaternion.identity,
+                _snapPositionIndicatorSize,
+                EventType.Repaint);
+
+            Handles.color = Color.black;
+            Handles.zTest = CompareFunction.LessEqual;
+
+            for (var i = 0; i < lineCount; i++)
+            {
+                var offset = i - halfLineCount;
+
+                var x = offset * _gridStep + center.x;
+                var z0 = center.z + halfLineCount * _gridStep;
+                var z1 = center.z - halfLineCount * _gridStep;
+                var p0 = new Vector3(x, center.y, z0);
+                var p1 = new Vector3(x, center.y, z1);
+                Handles.DrawAAPolyLine(p0, p1);
+
+                x = offset * _gridStep + center.z;
+                z0 = center.x + halfLineCount * _gridStep;
+                z1 = center.x - halfLineCount * _gridStep;
+                p0.Set(z0, center.y, x);
+                p1.Set(z1, center.y, x);
+                Handles.DrawAAPolyLine(p0, p1);
+            }
+        }
+    }
+
     private void OnGUI()
     {
         _so.Update();
-        EditorGUILayout.PropertyField(_propPolarGrid);
+        EditorGUILayout.PropertyField(_propGridType);
+        EditorGUILayout.PropertyField(_propGridStep);
         using (new EditorGUILayout.HorizontalScope())
         {
             using (new EditorGUILayout.VerticalScope())
             {
-                using (new EditorGUI.DisabledScope(_propPolarGrid.boolValue))
+                using (new EditorGUI.DisabledScope(_gridType == GridType.Polar))
                 {
-                    EditorGUILayout.PropertyField(_propGridStep);
                     EditorGUILayout.PropertyField(_propCartesianExtent);
                 }
             }
             using (new EditorGUILayout.VerticalScope())
             {
-                using (new EditorGUI.DisabledScope(_propPolarGrid.boolValue == false))
+                using (new EditorGUI.DisabledScope(_gridType == GridType.Cartesian))
                 {
                     EditorGUILayout.PropertyField(_propPolarExtent);
-                    EditorGUILayout.PropertyField(_propAngleStep);
-                    EditorGUILayout.PropertyField(_propDistanceStep);
+                    EditorGUILayout.PropertyField(_propUseAngularDivision);
+                    if (_useAngularDivision)
+                    {
+                        EditorGUILayout.PropertyField(_propAngularDivisions);
+                        _propAngularDivisions.intValue = Mathf.Max(3, _propAngularDivisions.intValue);
+                    }
+                    else
+                    {
+                        EditorGUILayout.PropertyField(_propAngleStep);
+                        _propAngleStep.floatValue = Mathf.Max(10, _propAngleStep.floatValue);
+                    }
                 }
             }
         }
