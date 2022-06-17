b916f488c0ccf709ddc67748fa1922194d95b013
Anton <rozum.ant@gmail.com>
2022-06-17 19:22:47 +0300

    wip

--- Assets/Scripts/Editor/SnapperTool.cs
+++ Assets/Scripts/Editor/SnapperTool.cs
@@ -2,22 +2,42 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-//TODO: 2. малювати цю сітку з виставленим кроком(навколо вибраних об'єктів/показувати позицію переміщення після снаппінгу)
 //TODO: 3. додати підтримку для полярної системи координат
 //TODO: 4. зробити сітку зберігатися між сесіями Юніті(закрив і відкрив Юніті - сітка зберігла свої параметри)
 
 public class SnapperTool : EditorWindow
 {
     private const string UNDO_SNAP = "snap objects";
-    
-    private float _gridStep = 1f;
-    private int _gridSize = 3;
+
+    [SerializeField] private bool _polarGrid;
+    [SerializeField] private float _angleStep;
+    [SerializeField] private float _distanceStep;
+    [SerializeField] private float _gridStep = 1f;
+    [SerializeField] private float _cartesianExtent = 3f;
+    [SerializeField] private float _polarExtent = 3f;
+    private float _snapPositionIndicatorSize = .1f;
+
+    private SerializedObject _so;
+    private SerializedProperty _propAngleStep;
+    private SerializedProperty _propDistanceStep;
+    private SerializedProperty _propPolarGrid;
+    private SerializedProperty _propGridStep;
+    private SerializedProperty _propCartesianExtent;
+    private SerializedProperty _propPolarExtent;
 
     [MenuItem("Tools/Snapper")]
     private static void OpenSnapperTool() => GetWindow<SnapperTool>("Snapper");
 
     private void OnEnable()
     {
+        _so = new SerializedObject(this);
+        _propAngleStep = _so.FindProperty(nameof(_angleStep));
+        _propDistanceStep = _so.FindProperty(nameof(_distanceStep));
+        _propPolarGrid = _so.FindProperty(nameof(_polarGrid));
+        _propGridStep = _so.FindProperty(nameof(_gridStep));
+        _propCartesianExtent = _so.FindProperty(nameof(_cartesianExtent));
+        _propPolarExtent = _so.FindProperty(nameof(_polarExtent));
+
         Selection.selectionChanged += Repaint;
         SceneView.duringSceneGui += DuringSceneGUI;
     }
@@ -32,28 +52,72 @@ public class SnapperTool : EditorWindow
     {
         if (Selection.gameObjects.Length == 0)
             return;
-        Handles.zTest = CompareFunction.Less;
-        var offset = _gridStep * .5f;
-        Handles.color = Color.black;
-
-        foreach (var obj in Selection.gameObjects)
+        if (Event.current.type != EventType.Repaint)
+            return;
+        
+        if (_propPolarGrid.boolValue)
         {
-            var pos = obj.transform.position;
-            var center = GetSnappedPosition(pos);
+            var circlesAmount = Mathf.RoundToInt((_polarExtent * 2) / _distanceStep);
+            var linesCount = _angleStep > 0 ? Mathf.FloorToInt(360 / _angleStep) : 0;
+            var rotationAngle = new Vector3(0, _angleStep, 0);
+            var rotation = Matrix4x4.Rotate(Quaternion.Euler(rotationAngle));
 
-            for (var i = 0; i < _gridSize * 2 + 1; i++)
+            Handles.color = Color.black;
+            Handles.zTest = CompareFunction.LessEqual;
+            for (var i = 1; i <= circlesAmount; i++)
             {
-                // var y = (i - _gridSize) * _gridStep + center.y;
-                
-                var z = (i-_gridSize)*_gridStep + center.z;
-                var p1 = new Vector3(center.x - _gridSize * _gridStep - offset, center.y, z);
-                var p2 = new Vector3(center.x + _gridSize * _gridStep + offset, center.y, z);
-                Handles.DrawLine(p1, p2);
+                Handles.DrawWireDisc(Vector3.zero, Vector3.up, _distanceStep * i);
+            }
 
-                var x = (i-_gridSize) * _gridStep + center.x;
-                p1.Set(x, center.y, center.z + _gridSize * _gridStep + offset);
-                p2.Set(x, center.y, center.z - _gridSize * _gridStep - offset);
-                Handles.DrawLine(p1, p2);
+            var nextPoint = Vector3.right * (circlesAmount * _distanceStep);
+            for (var i = 0; i < linesCount; i++)
+            {
+                Handles.DrawAAPolyLine(Vector3.zero, nextPoint);
+                nextPoint = rotation * nextPoint;
+            }
+        }
+        else
+        {
+            var lineCount = Mathf.RoundToInt((_cartesianExtent * 2) / _gridStep);
+            if (lineCount % 2 == 0)
+                lineCount++;
+            var halfLineCount = lineCount / 2;
+            
+            foreach (var obj in Selection.gameObjects)
+            {
+                var pos = obj.transform.position;
+                var center = pos.Round(_gridStep);
+
+                Handles.color = Color.red;
+                Handles.zTest = CompareFunction.Always;
+                
+                Handles.SphereHandleCap(0,
+                    center,
+                    Quaternion.identity,
+                    _snapPositionIndicatorSize,
+                    EventType.Repaint);
+
+                Handles.color = Color.black;
+                Handles.zTest = CompareFunction.LessEqual;
+
+                for (var i = 0; i < lineCount; i++)
+                {
+                    var offset = i - halfLineCount;
+
+                    var x = offset * _gridStep + center.x;
+                    var z0 = center.z + halfLineCount * _gridStep;
+                    var z1 = center.z - halfLineCount * _gridStep;
+                    var p0 = new Vector3(x, center.y, z0);
+                    var p1 = new Vector3(x, center.y, z1);
+                    Handles.DrawAAPolyLine(p0, p1);
+
+                    x = offset * _gridStep + center.z;
+                    z0 = center.x + halfLineCount * _gridStep;
+                    z1 = center.x - halfLineCount * _gridStep;
+                    p0.Set(z0, center.y, x);
+                    p1.Set(z1, center.y, x);
+                    Handles.DrawAAPolyLine(p0, p1);
+                }
             }
         }
 
@@ -62,8 +126,29 @@ public class SnapperTool : EditorWindow
 
     private void OnGUI()
     {
-        _gridStep = EditorGUILayout.FloatField("Grid step", _gridStep);
-        _gridSize = EditorGUILayout.IntField("Grid size", _gridSize);
+        _so.Update();
+        EditorGUILayout.PropertyField(_propPolarGrid);
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            using (new EditorGUILayout.VerticalScope())
+            {
+                using (new EditorGUI.DisabledScope(_propPolarGrid.boolValue))
+                {
+                    EditorGUILayout.PropertyField(_propGridStep);
+                    EditorGUILayout.PropertyField(_propCartesianExtent);
+                }
+            }
+            using (new EditorGUILayout.VerticalScope())
+            {
+                using (new EditorGUI.DisabledScope(_propPolarGrid.boolValue == false))
+                {
+                    EditorGUILayout.PropertyField(_propPolarExtent);
+                    EditorGUILayout.PropertyField(_propAngleStep);
+                    EditorGUILayout.PropertyField(_propDistanceStep);
+                }
+            }
+        }
+        _so.ApplyModifiedProperties();
         using (new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0))
         {
             if (GUILayout.Button("SnapSelection"))
@@ -80,29 +165,7 @@ public class SnapperTool : EditorWindow
             Undo.RecordObject(go.transform, UNDO_SNAP);
 
             var newPosition = go.transform.position;
-            go.transform.position = GetSnappedPosition(newPosition);
+            go.transform.position = newPosition.Round(_gridStep);
         }
-    }
-
-    private Vector3 GetSnappedPosition(Vector3 pos)
-    {
-        var newPosition = pos;
-        for (var i = 0; i < 3; i++)
-        {
-            var coordinate = newPosition[i];
-            var delta = coordinate % _gridStep;
-            if (delta > _gridStep * .5f)
-            {
-                coordinate += _gridStep - delta;
-            }
-            else
-            {
-                coordinate -= delta;
-            }
-
-            newPosition[i] = coordinate;
-        }
-
-        return newPosition;
     }
 }
